3eb96ae46dcf55c9f0ab5127261b528576942630
LenX <lenxdata@gmail.com>
2021-06-20 13:28:23 +0200

    add ANN.R

---
+++ ANN.R
@@ -0,0 +1,17 @@
+### Artificial Neural Network ###   06.20.2021
+
+# Load libraries
+library(tidyverse)
+library(ggplot2)
+suppressPackageStartupMessages(library(keras))
+library(tensorflow)
+
+
+# Neural Network on Local Features
+
+# load data from "Transformation.R"
+str(train_lf) # train data, local features
+str(valid_lf) # validation data, local features
+
+
+
