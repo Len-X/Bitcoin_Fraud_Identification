27aa664b8fc974a956dcf1e9187a9fb328bd2a88
LenX <lenxdata@gmail.com>
2021-06-20 16:26:16 +0200

    build ANN model

--- ANN.R
+++ ANN.R
@@ -29,14 +29,108 @@ levels(train_lf$class) <- c(1, 0)
 levels(valid_lf$class) <- c(1, 0)
 # alternative way
 # levels(train_lf$class)[levels(train_lf$class)==2] <- 0
+
 table(train_lf$class)
 table(valid_lf$class)
 
-y_train <- train_lf$class  # outcome/target variable
-y_valid <- valid_lf$class  # outcome/target variable
+# y_train <- as.numeric(train_lf$class)
+# y_valid <- as.numeric(valid_lf$class)
+
+y_train <- to_categorical(train_lf$class)
+y_valid <- to_categorical(valid_lf$class)
+
+# y_train <- train_lf$class %>% as.matrix() # outcome/target variable
+# y_valid <- valid_lf$class %>% as.matrix() # outcome/target variable
 
 # set "dimnames" to "NULL"
 dimnames(x_train) <- NULL
 dimnames(x_valid) <- NULL
+
+## Build the NN model ##
+
+set.seed(2021)
+
+# initialize a sequential model
+model <- keras_model_sequential()
+
+# set model
+model %>%
+  layer_dense(units = 186, activation = "relu", input_shape = ncol(x_train)) %>%
+  layer_dense(units = 62, activation = "relu") %>%
+  layer_dense(units = 2, activation = "softmax")
+
+# input_shape - number of input variables
+# units = 2 - indicates 2 classes
+
+# summary of a model
+summary(model)
+
+# model configuration
+get_config(model)
+
+# layer configuration
+get_layer(model, index = 1)
+
+# used to retrieve a flattened list of the model’s layers
+model$layers
+
+# list the input tensors
+model$inputs
+
+# list the output tensors
+model$outputs
+
+# compile the model
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = "adam",
+  metrics = "accuracy")
+
+# fit the model and store the fitting history
+history <- model %>% fit(
+  x_train, 
+  y_train, 
+  epochs = 50, 
+  batch_size = 32, 
+  validation_split = 0.2,
+  verbose = 1)
+
+# plot the history
+plot(history)
+
+# make predictions for the validation data
+predictions <- model %>% predict_classes(x_valid, batch_size = 128)
+
+# Confusion matrix
+table(valid_lf$class, predictions)
+
+# swap levels in predictions. Make 1 first
+predictions <- relevel(predictions, "1")
+table(predictions)
+
+conf_matrix <- confusionMatrix(valid_lf$class, predictions, positive = "1")
+conf_matrix
+
+# evaluate the model
+score <- model %>% evaluate(x_valid, y_valid, batch_size = 128)
+
+# Print the score
+print(score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
