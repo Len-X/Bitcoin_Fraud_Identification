9db11aa59f13aaa81db601965c07d332972cc3f3
LenX <lenxdata@gmail.com>
2021-09-11 21:25:18 +0200

    add model_4

--- ANN.R
+++ ANN.R
@@ -260,6 +260,67 @@ ggroc(list(Train = roc_train_3, Validation = roc_test_3), legacy.axes = TRUE) +
 auc(roc_train_3) 
 auc(roc_test_3) 
 
+
+# ANN 4th model with Class Weights
+# 100% of class 1 (fraud) and 11% of class 0 (non-fraud)
+
+model_4 <- keras_model_sequential() # 4th model
+# use same architecture as with baseline
+model_4 %>%
+  layer_dense(units = 128, activation = "relu", input_shape = ncol(x_train)) %>%
+  layer_dense(units = 64, activation = "relu") %>%
+  layer_dense(units = 2, activation = "softmax")
+
+summary(model_4)
+
+# compile the 4th model
+model_4 %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = "adam",
+  metrics = "accuracy")
+
+# fit the baseline model and store the fitting history
+history_4 <- model_4 %>% fit(
+  x_train, 
+  y_train, 
+  epochs = 100, 
+  batch_size = 128,
+  validation_data = list(x_valid, y_valid),
+  class_weight = list("1"=100,"0"=10), # class weights
+  verbose = 1)
+
+print(history_4)
+plot(history_4)
+
+# make predictions for the validation data, 4th model
+predictions_4 <- model_4 %>% predict_classes(x_valid, batch_size = 128)
+probabilities_4 <- model_4 %>% predict_proba(x_valid) %>% as.data.frame()
+probabilities_train_4 <- model_4 %>% predict_proba(x_train) %>% as.data.frame()
+
+# swap levels in predictions. Make 1 first
+predictions_4 <- relevel((as.factor(predictions_4)), "1")
+table(predictions_4)
+
+# confusion matrix
+conf_matrix_4 <- confusionMatrix(valid_lf$class, predictions_4)
+conf_matrix_4
+
+# evaluation by class
+evaluation_4 <- data.frame(conf_matrix_4$byClass)
+evaluation_4
+
+score_4 <- model_4 %>% evaluate(x_valid, y_valid, batch_size = 128)
+print(score_4)
+
+# ROC Test for 3rd model
+roc_train_4 <- roc(train_lf$class, probabilities_train_4$V1)
+roc_test_4 <- roc(valid_lf$class, probabilities_4$V1)
+ggroc(list(Train = roc_train_4, Validation = roc_test_4), legacy.axes = TRUE) +
+  ggtitle("ROC of 4th ANN model with Local features") +
+  labs(color = "")
+auc(roc_train_4) 
+auc(roc_test_4)
+
 # safe and load the models
 
 # baseline model
@@ -285,6 +346,14 @@ model_3 <- load_model_hdf5("3rd_model.h5")
 # save history as df and safe to csv
 history_df_3 <- as.data.frame(history_3)
 write.csv(history_df_3, "history_df_3.csv", row.names = FALSE)
+
+# 4th model
+save_model_hdf5(model_4, "4th_model.h5")
+model_4 <- load_model_hdf5("4th_model.h5")
+
+# save history as df and safe to csv
+history_df_4 <- as.data.frame(history_4)
+write.csv(history_df_4, "history_df_4.csv", row.names = FALSE)
 
 
 ## Compare the models
