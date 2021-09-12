9edef4fa6290b65a150f9468486354955009e9ba
LenX <lenxdata@gmail.com>
2021-09-12 18:26:58 +0200

    add AE on AF

--- Autoencoder.R
+++ Autoencoder.R
@@ -8,3 +8,86 @@ suppressPackageStartupMessages(library(keras))
 
 # Autoencoder on All Features (AF)
 
+# Data Preprocessing
+
+# load data from "Transformation.R"
+x_train <- train_af %>%  # predictor variables
+  select(-class) %>%
+  as.matrix()
+
+x_valid <- valid_af %>%  # predictor variables
+  select(-class) %>%
+  as.matrix()
+
+x_test <- test_af %>%  # predictor variables
+  select(-class) %>%
+  as.matrix()
+
+# outcome variable
+y_train <- train_af$class
+y_valid <- valid_af$class
+y_test <- test_af$class
+
+set.seed(2021)
+
+# set AE model
+model_ae <- keras_model_sequential()
+model_ae %>%
+  layer_dense(units = 60, activation = "tanh", input_shape = ncol(x_train)) %>%
+  layer_dense(units = 20, activation = "tanh", name = "bottleneck") %>%
+  layer_dense(units = 60, activation = "tanh") %>%
+  layer_dense(units = ncol(x_train))
+
+# view model layers
+summary(model_ae)
+
+# compile model
+model_ae %>% compile(
+  loss = "mean_squared_error", 
+  optimizer = "adam"
+)
+
+# fit model
+model_ae %>% fit(
+  x = x_train, 
+  y = x_train, 
+  epochs = 500,  # try 100, 500, 1000
+  validation_data = list(x_valid, x_valid),
+  verbose = 1
+)
+
+# evaluate the performance of the model
+mse_ae_train <- evaluate(model_ae, x_train, x_train)
+mse_ae_train
+
+mse_ae_valid <- evaluate(model_ae, x_valid, x_valid)
+mse_ae_valid
+
+mse_ae_test <- evaluate(model_ae, x_test, x_test)
+mse_ae_test
+
+# extract the bottleneck layer
+intermediate_layer_model <- keras_model(inputs = model_ae$input, 
+                                        outputs = get_layer(model_ae, "bottleneck")$output)
+# train prediction
+intermediate_output_train <- predict(intermediate_layer_model, x_train)
+intermediate_output_train
+# validation prediction
+intermediate_output_valid <- predict(intermediate_layer_model, x_valid)
+intermediate_output_valid
+# test prediction
+intermediate_output_test <- predict(intermediate_layer_model, x_test)
+intermediate_output_test
+
+# combine class and AE-derived train features
+df_ae_train_af <- cbind(y_train, intermediate_output_train)
+# combine class and AE-derived validation features
+df_ae_valid_af <- cbind(y_valid, intermediate_output_valid)
+# combine class and AE-derived test features
+df_ae_test_af <- cbind(y_test, intermediate_output_test)
+
+# save to csv combined AE hidden layer output
+write.csv(df_ae_train_af, "ae_20_AF_train.csv", row.names = FALSE)
+write.csv(df_ae_valid_af, "ae_20_AF_valid.csv", row.names = FALSE)
+write.csv(df_ae_test_af, "ae_20_AF_test.csv", row.names = FALSE)
+
