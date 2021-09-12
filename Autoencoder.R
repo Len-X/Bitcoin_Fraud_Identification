8cf20cd1997259679deee52a5031bdbc983192ee
LenX <lenxdata@gmail.com>
2021-09-12 17:56:44 +0200

    create and add new file Autoencoder.R

---
+++ Autoencoder.R
@@ -0,0 +1,10 @@
+### Autoencoder ### 09.12.2021
+# created from Feature_engineering.R
+
+# Load libraries
+library(tidyverse)
+library(ggplot2)
+suppressPackageStartupMessages(library(keras))
+
+# Autoencoder on All Features (AF)
+
