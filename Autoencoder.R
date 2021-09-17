df7531c6efb9a9627be332ab8549f8c96baa1d65
LenX <lenxdata@gmail.com>
2021-09-17 12:10:40 +0200

    extract 20 AE features from AF

--- Autoencoder.R
+++ Autoencoder.R
@@ -51,20 +51,20 @@ model_ae %>% compile(
 model_ae %>% fit(
   x = x_train, 
   y = x_train, 
-  epochs = 500,  # try 100, 500, 1000
+  epochs = 100,  # try 100, 500, 1000
   validation_data = list(x_valid, x_valid),
   verbose = 1
 )
 
 # evaluate the performance of the model
 mse_ae_train <- evaluate(model_ae, x_train, x_train)
-mse_ae_train
+mse_ae_train # 0.2546721
 
 mse_ae_valid <- evaluate(model_ae, x_valid, x_valid)
-mse_ae_valid
+mse_ae_valid # 1.056373
 
 mse_ae_test <- evaluate(model_ae, x_test, x_test)
-mse_ae_test
+mse_ae_test # 0.9320067
 
 # extract the bottleneck layer
 intermediate_layer_model <- keras_model(inputs = model_ae$input, 
@@ -79,12 +79,29 @@ intermediate_output_valid
 intermediate_output_test <- predict(intermediate_layer_model, x_test)
 intermediate_output_test
 
+# set anonymous variable names
+col_prefix <- ("V")
+n = 20
+names_list <- unlist(mapply(function(x,y) 
+  paste(x, seq(1,y), sep="_"), col_prefix, n))
+
 # combine class and AE-derived train features
-df_ae_train_af <- cbind(y_train, intermediate_output_train)
+ae_train_af <- cbind(y_train, intermediate_output_train)
+df_ae_train_af <- ae_train_af %>%
+  as_tibble() %>%
+  setNames(c("class", names_list))
+
 # combine class and AE-derived validation features
-df_ae_valid_af <- cbind(y_valid, intermediate_output_valid)
+ae_valid_af <- cbind(y_valid, intermediate_output_valid)
+df_ae_valid_af <- ae_valid_af %>%
+  as_tibble() %>%
+  setNames(c("class", names_list))
+
 # combine class and AE-derived test features
-df_ae_test_af <- cbind(y_test, intermediate_output_test)
+ae_test_af <- cbind(y_test, intermediate_output_test)
+df_ae_test_af <- ae_test_af %>%
+  as_tibble() %>%
+  setNames(c("class", names_list))
 
 # save to csv combined AE hidden layer output
 write.csv(df_ae_train_af, "ae_20_AF_train.csv", row.names = FALSE)
