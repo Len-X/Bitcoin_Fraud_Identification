bc64b27e74f180987c4ca164fbcbbad6d0957ae5
LenX <lenxdata@gmail.com>
2021-12-12 16:14:26 +0100

    LR on test data

--- Best_models_test.R
+++ Best_models_test.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(ggcorrplot)
 library(caret)
 library(pROC)
+library(randomForest)
 
 ### Logistic Regression with AF + LVQ + CORR + IF (7 features) ###
 
@@ -46,6 +47,32 @@ lvq_glm_preds_test[lvq_glm_probs_test >.5 ] = 2
 lvq_glm_preds <- as.factor(lvq_glm_preds)
 # set levels for predictions test data
 lvq_glm_preds_test <- as.factor(lvq_glm_preds_test)
+
+# Classification Matrix test data
+conf_matrix_lvq <- confusionMatrix(lvq_glm_preds_test, lvq_test_outcome$class, positive = "1")
+conf_matrix_lvq
+
+# glm model evaluation on test data
+lvq_glm_evaluation <- data.frame(conf_matrix_lvq$byClass)
+lvq_glm_evaluation
+
+# AUC/ROC
+
+# ROC Train
+fit_lvq <- fitted(glm_lvq)
+roc_lvq_train <- roc(lvq_train$class, fit_lvq)
+# ROC Validation
+roc_lvq_validation <- roc(lvq_validation_outcome$class, lvq_glm_probs)
+# ROC Test
+roc_lvq_test <- roc(lvq_test_outcome$class, lvq_glm_probs_test)
+ggroc(list(train=roc_lvq_train, validation=roc_lvq_validation, test=roc_lvq_test), legacy.axes = TRUE) +
+  ggtitle("ROC of best Logistic Regression model with LVQ on all features, Highly Correlated and unimportant features removed") +
+  labs(color = "")
+
+# AUC
+auc(roc_lvq_train) # 0.943
+auc(roc_lvq_validation) # 0.9045
+auc(roc_lvq_test) # 0.8265
 
 
 
